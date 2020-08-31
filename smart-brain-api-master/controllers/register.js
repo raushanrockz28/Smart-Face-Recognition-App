3817505db32bfae6d1f504e7bef3438e5613b0a6
rockz28 <48850884+rockz28@users.noreply.github.com>
2020-08-31 15:27:00 +0530

    init commit

---
+++ smart-brain-api-master/controllers/register.js
@@ -0,0 +1,43 @@
+const handleRegister = (req, res, db, bcrypt) => {
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+  console.log('body',req.body)
+  const hash = bcrypt.hashSync(password);
+  console.log('hash',hash)
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        console.log('loginEmail', loginEmail)
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            // joined: new Date()
+          })
+          .then(user => {
+            console.log('user',user)
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+
+      
+      .catch(trx.rollback)
+      
+    })
+    .catch(err => res.status(400).json('unable to register'))
+}
+
+module.exports = {
+  handleRegister: handleRegister
+};
+
+
